fdc3764e34b56f0702b39e3dc3034d3a7fca7127
Dr Nic Williams <drnicwilliams@gmail.com>
2024-05-13 22:02:17 +1000

    Add promptcraft 0.1.0

--- promptcraft.rb
+++ promptcraft.rb
@@ -1,17 +1,21 @@
 class Promptcraft < Formula
   desc "Try out new system prompts on your existing AI conversations. Over and over until you're happy."
   homepage "https://github.com/drnic/promptcraft"
-  url "https://rubygems.org/downloads/promptcraft-0.1.0.gem"
-  sha256 "0fc084c6d9c04936d58421976257573cfc7f82a321f758feb25223558d6fbf5f"
+  version "0.1.0"
+  url "https://github.com/drnic/promptcraft/releases/download/v0.1.0/promptcraft-0.1.0.tar.xz"
+  sha256 "2ced998916398902db0345064a118df10989411e4af7c1188ea966f0cc8f0f22"
 
   depends_on "ruby@3.3"
 
   def install
-    libexec = Pathname.new("#{HOMEBREW_PREFIX}/libexec")
-    libexec.install "promptcraft-0.1.0.gem", "Gemfile.lock"
-    ENV["GEM_HOME"] = libexec
-    system "bundle", "install", "--path=#{libexec}"
-    system "gem", "install", libexec/"promptcraft-0.1.0.gem"
-    bin.write_exec_script Dir["#{libexec}/bin/*"]
+    # Extract all files to libexec, which is a common Homebrew practice for third-party tools
+    libexec.install Dir["*"]
+    # Create a symbolic link for the executable in Homebrew's bin directory
+    bin.install_symlink "#{libexec}/exe/promptcraft" => "promptcraft"
+  end
+
+  test do
+    # Simple test to check the version or a help command
+    system "#{bin}/promptcraft", "--version"
   end
 end
