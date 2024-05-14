16b83a433bfee74cd3aeec3e760e478b87f8a4fd
Dr Nic Williams <drnicwilliams@gmail.com>
2024-05-14 16:19:40 +1000

    Bump promptcraft v0.1.1

--- promptcraft.rb
+++ promptcraft.rb
@@ -1,9 +1,9 @@
 class Promptcraft < Formula
   desc "Try out new system prompts on your existing AI conversations. Over and over until you're happy."
   homepage "https://github.com/drnic/promptcraft"
-  version "0.1.0"
-  url "https://github.com/drnic/promptcraft/releases/download/v0.1.0/promptcraft-0.1.0.tar.xz"
-  sha256 "2ced998916398902db0345064a118df10989411e4af7c1188ea966f0cc8f0f22"
+  version "0.1.1"
+  url "https://github.com/drnic/promptcraft/releases/download/v0.1.1/promptcraft-0.1.1.tar.xz"
+  sha256 "e932cde5d7af5f7dbd0a35dc31264787566741acd394c7488e2abd419d4cbab4"
 
   depends_on "ruby@3.3"
 
